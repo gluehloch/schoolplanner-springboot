823b88ca944a9c6c49a4740409c512fadffef0c0
andrewinkler <andre.winkler@web.de>
2020-02-16 16:27:36 +0100

    TODO: Try to enable Spring-Web-Security.

---
+++ src/main/java/de/awtools/schoolplanner/SchoolplannerApplication.java
@@ -0,0 +1,14 @@
+package de.awtools.schoolplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class SchoolplannerApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolplannerApplication.class, args);
+    }
+
+}
